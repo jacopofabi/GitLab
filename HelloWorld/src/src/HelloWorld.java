abbd1eb6e026e9d5b7122fea9b7d320ddb7eb1bd
jacopofabi <jacopo.fabi1997@gmail.com>
2021-03-14 12:59:15 +0100

    new line added in the code

--- HelloWorld/src/src/HelloWorld.java
+++ HelloWorld/src/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//print 'Hello World' on the screen
 		System.out.println("Hello World");
+		System.out.println("The code works");
 	}
 }
