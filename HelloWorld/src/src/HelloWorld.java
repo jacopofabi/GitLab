2ac2f7a190dcb5c03b55ddac6c4c960c3f927fe2
jacopofabi <jacopo.fabi1997@gmail.com>
2021-03-14 12:57:25 +0100

    correct 'Hello Word' to 'Hello World'

--- HelloWorld/src/src/HelloWorld.java
+++ HelloWorld/src/src/HelloWorld.java
@@ -3,6 +3,6 @@ package src;
 public class HelloWorld {
 	public static void main(String[] args) {
 		//print 'Hello World' on the screen
-		System.out.println("Hello Word");
+		System.out.println("Hello World");
 	}
 }
